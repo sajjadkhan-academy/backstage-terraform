686ddb64b4ac29b804cb822a72f0ace473932384
Sajjad Khan <sk7698277@gmail.com>
2026-01-29 14:00:19 +0000

    Provisioning GCP Bucket: test-342342342

---
+++ terraform/resources/test-342342342/variables.tf
@@ -0,0 +1,32 @@
+# Variables for GCP Storage Bucket
+# Values are templated from Backstage
+
+variable "bucket_name" {
+  type        = string
+  description = "Name of the storage bucket"
+  default     = "test-342342342"
+}
+
+variable "project_id" {
+  type        = string
+  description = "GCP Project ID"
+  default     = "fast-pagoda-474409-c7"
+}
+
+variable "location" {
+  type        = string
+  description = "Bucket location"
+  default     = "us-central1"
+}
+
+variable "storage_class" {
+  type        = string
+  description = "Storage class for the bucket"
+  default     = "STANDARD"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment tag"
+  default     = "dev"
+}
