0e7c59813cb8dde6f6ed463d7b7ca5812c26913a
Sajjad Khan <sk7698277@gmail.com>
2026-01-31 19:59:20 +0000

    Update bucket.tf

--- teams/team-alpha/teams/team-alpha/bucket.tf
+++ teams/team-alpha/teams/team-alpha/bucket.tf
@@ -3,15 +3,3 @@
 # Team: team-alpha
 # Created by Backstage Software Template
 
-module "bucket_test_342342342" {
-  source = "github.com/sajjadkhan-academy/backstage-terraform//modules/gcp-bucket?ref=main"
-
-  name       = "test-342342342"
-  project_id = "fast-pagoda-474409-c7"
-  location   = "US"
-
-  labels = {
-    managed_by = "backstage"
-    team       = "team-alpha"
-  }
-}
